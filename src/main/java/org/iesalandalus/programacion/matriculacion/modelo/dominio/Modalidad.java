3415ccdaadfb5083122e39bb2cab6b113b8b3ccc
LAHROURHamza <hamzalahrour3@gmail.com>
2025-03-02 20:45:20 +0100

    Modalidadd

---
+++ src/main/java/org/iesalandalus/programacion/matriculacion/modelo/dominio/Modalidad.java
@@ -0,0 +1,24 @@
+package org.iesalandalus.programacion.matriculacion.modelo.dominio;
+
+public enum Modalidad {
+
+    PRESENCIAL("PRESENCIAL"),SEMIPRESENCIAL("SEMIPRESENCIAL");
+
+    String cadenaAMostrar;
+
+    Modalidad(String cadenaAMostrar) {
+        this.cadenaAMostrar = cadenaAMostrar;
+    }
+
+    public String imprimir(){
+
+        return ordinal()+ " .- "+cadenaAMostrar;
+    }
+
+    @Override
+    public String toString() {
+        return "Modalidad{" +
+                "cadenaAMostrar='" + cadenaAMostrar + '\'' +
+                '}';
+    }
+}
